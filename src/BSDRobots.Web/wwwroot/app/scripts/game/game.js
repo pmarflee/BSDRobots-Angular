4a56bd98163688a670535b501c96da075b969cfd
Paul Marfleet <paul.marfleet@gmail.com>
2015-07-19 18:20:17 +0100

    Fixed bug with game ending when player collides with a robot.  Show 'Game Over' message when game ends.

--- src/BSDRobots.Web/wwwroot/app/scripts/game/game.js
+++ src/BSDRobots.Web/wwwroot/app/scripts/game/game.js
@@ -3,11 +3,16 @@
 angular
     .module('Game', ['Grid'])
     .service('GameManager', function ($q, $timeout, $log, GridService) {
+        var gameSize = GridService.getSize(),
+            cellSize = GridService.getCellSize();
+
         this.grid = GridService.grid;
         this.player = GridService.player;
-        this.gameSize = GridService.getSize();
-        this.cellSize = GridService.getCellSize();
-
+        this.gameSize = gameSize;
+        this.cellSize = cellSize;
+        this.gameWidth = (gameSize.x + 1) * cellSize;
+        this.gameHeight = (gameSize.y + 1) * cellSize;
+        
         this.reinit = function () {
             this.gameOver = false;
             this.win = false;
@@ -30,8 +35,9 @@ angular
             GridService.moveRobots();
             for (var i = 0; i < GridService.robots.length; i++) {
                 var robot = GridService.robots[i];
-                if (this.player.hasSamePositionAs(robot.x)) {
+                if (this.player.hasSamePositionAs(robot)) {
                     this.player.isAlive = false;
+                    this.gameOver = true;
                 }
                 for (var j = 0; j < GridService.robots.length; j++) {
                     var otherRobot = GridService.robots[j];
