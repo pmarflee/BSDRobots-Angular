e60667c154eeea3b28e8d6146544782bcde1e2f0
Paul Marfleet <paul.marfleet@gmail.com>
2015-07-25 23:09:18 +0100

    Added bootstrap menu bar.  Use the angular-bootstrap bower package to remove the dependency on jQuery

--- src/BSDRobots.Web/Gulpfile.js
+++ src/BSDRobots.Web/Gulpfile.js
@@ -10,55 +10,59 @@ var fs = require("fs"),
     ngAnnotate = require('gulp-ng-annotate'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
+    minifyCSS = require('gulp-minify-css'),
     sourcemaps = require('gulp-sourcemaps'),
     bower = require('gulp-bower'),
     app_bundle = 'app.min.js',
-    jquery_bundle = 'jquery.min.js',
-    bootstrap_bundle = 'bootstrap.min.js';
+    angular_bundle = 'angular.min.js',
+    angular_bootstrap_bundle = 'angular-bootstrap.min.js';
     
 eval("var project = " + fs.readFileSync("./project.json"));
 
 var paths = {
     app_src: project.webroot + '/app/**/*.js',
-    app_out: project.webroot + '/build',
-    app_bundle: project.webroot + '/build/' + app_bundle,
-    jquery_src: [
-        'bower_components/jquery/dist/jquery.min.js',
-        'bower_components/jquery-validation/dist/jquery.validate.min.js',
-        'bower_components/jquery-validation-unobtrusive/jquery.validate.unobtrusive.min.js'
+    app_out: project.webroot + '/build/scripts',
+    app_bundle: project.webroot + '/build/scripts/' + app_bundle,
+    app_css: project.webroot + '/app/styles/main.css',
+    angular_src: [
+        'bower_components/angular/angular.min.js'
     ],
-    jquery_bundle: 'Scripts/jquery-bundle.min.js',
-    bootstrap_src: [
-        'bower_components/bootstrap/dist/js/bootstrap.min.js',
-        'bower_components/respond-minmax/dest/respond.min.js'
+    angular_bundle: project.webroot + '/build/' + angular_bundle,
+    angular_bootstrap_src: [
+        'bower_components/angular-bootstrap/ui-bootstrap.min.js',
+        'bower_components/angular-animate/angular-animate.min.js'
     ],
-    bootstrap_bundle: project.webroot + '/build/' + bootstrap_bundle,
-    bootstrap_css: 'bower_components/bootstrap/dist/css/bootstrap.css'
+    angular_bootstrap_bundle: project.webroot + '/build/' + angular_bootstrap_bundle,
+    bootstrap_css: 'bower_components/bootstrap-css-only/css/bootstrap.css',
+    bootstrap_fonts: 'bower_components/bootstrap-css-only/fonts/*.*',
+    fonts_out: project.webroot + '/build/content/dist/fonts',
+    css_out: project.webroot + '/build/content/dist/css'
 };
 
-gulp.task('default', ['vendor-scripts', 'app-scripts']);
+gulp.task('default', ['vendor-scripts', 'app-scripts', 'styles']);
 
-gulp.task('vendor-scripts', ['jquery-bundle', 'bootstrap-bundle']);
+gulp.task('vendor-scripts', ['angular-bundle', 'angular-bootstrap-bundle']);
 
 gulp.task('clean-app-scripts', function () {
     del([paths.app_bundle]);
 });
 
 gulp.task('clean-vendor-scripts', function () {
-    del([paths.jquery_bundle, paths.bootstrap_bundle]);
+    del([paths.angular_bundle, paths.angular_bootstrap_bundle]);
 });
 
-gulp.task('jquery-bundle', ['clean-vendor-scripts', 'bower-restore'], function () {
-    return gulp.src(paths.jquery_src)
-     .pipe(concat(jquery_bundle))
+gulp.task('angular-bundle', ['clean-vendor-scripts', 'bower-restore'], function () {
+    return gulp.src(paths.angular_src)
+     .pipe(concat(angular_bundle))
+     .pipe(sourcemaps.write())
      .pipe(gulp.dest(paths.app_out))
      .on('error', gutil.log);
 });
 
-gulp.task('bootstrap-bundle', ['clean-vendor-scripts', 'bower-restore'], function () {
-    return gulp.src(paths.bootstrap_src)
+gulp.task('angular-bootstrap-bundle', ['clean-vendor-scripts', 'bower-restore'], function () {
+    return gulp.src(paths.angular_bootstrap_src)
      .pipe(sourcemaps.init())
-     .pipe(concat(bootstrap_bundle))
+     .pipe(concat(angular_bootstrap_bundle))
      .pipe(sourcemaps.write())
      .pipe(gulp.dest(paths.app_out))
      .on('error', gutil.log);
@@ -74,6 +78,27 @@ gulp.task('app-scripts', ['clean-app-scripts'], function () {
     .pipe(gulp.dest(paths.app_out))
     .on('error', gutil.log);
 })
+
+gulp.task('clean-styles', function () {
+    del([paths.fonts_out, paths.css_out]);
+});
+
+gulp.task('css', ['clean-styles', 'bower-restore'], function () {
+    return gulp.src([paths.bootstrap_css, paths.app_css])
+     .pipe(concat('app.css'))
+     .pipe(gulp.dest(paths.css_out))
+     .pipe(minifyCSS())
+     .pipe(concat('app.min.css'))
+     .pipe(gulp.dest(paths.css_out));
+});
+
+gulp.task('fonts', ['clean-styles', 'bower-restore'], function () {
+    return gulp.src(paths.bootstrap_fonts).pipe(gulp.dest(paths.fonts_out));
+});
+
+gulp.task('styles', ['css', 'fonts'], function () {
+
+});
 
 gulp.task('bower-restore', function () {
     return bower();
